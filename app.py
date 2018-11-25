b0f95c649fac2086ff2e3e15e846e87a81e0b2f1
csvk <chanda.souvik@gmail.com>
2018-11-25 23:16:25 +0530

    Secret key

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, redirect, url_for, request, flash
+#from logging import DEBUG
 
 app = Flask(__name__)
+#app.logger.setLevel(DEBUG)
+
+bookmarks = []
+app.config['SECRET KEY'] = "\xb5\xab\xf2\x08fF\x08</\xa2\xceD\\\x00\x00\x8fa\x8f\xd5\xb4hY[r\xc1\xe9\x87\xb7V\xa9\xe8\xaa"
+
+
+def store_bookmark(url):
+    bookmarks.append(dict(
+        url=url,
+        user='Souvik Chanda',
+        date=datetime.utcnow()
+    ))
 
 
 class User:
@@ -17,8 +31,15 @@ def index():
     return render_template("index.html", user=User('Souvik', 'Chanda'))
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
+    if request.method == 'POST':
+        url = request.form["url"]
+        store_bookmark(url)
+        #app.logger.debug('stored url: ' + url)
+        flash('stored url: ' + url)
+        return redirect(url_for('index'))
+
     return render_template("add.html")
 
 
