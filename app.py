bfe9ce1e2f9dbdde30d3f2a63d1fab5b5db5df06
csvk <chanda.souvik@gmail.com>
2018-11-25 12:50:42 +0530

    Blocks used

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
+class User:
+    def __init__(self, fname, lname):
+        self.fname, self.lname = fname, lname
+
+    def initials(self):
+        return "{}.{}.".format(self.fname[0], self.lname[0])
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", website=["Kuttus", "Tintin"])
+    return render_template("index.html", user=User('Souvik', 'Chanda'))
+
+@app.route('/add')
+def add():
+    return render_template("add.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
